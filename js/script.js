4304db711e6a59adc66e3f23c774fee75a76d7e4
Riki420 <girottiriccardo1998@gmail.com>
2021-07-05 18:02:24 +0200

    create variabili fizz e buzz, modificato il ciclo

--- js/script.js
+++ js/script.js
@@ -2,17 +2,22 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-for (var i = 1; i <= 100; i++){
+var f = "Fizz";
+var b = "Buzz";
+var fb = f + b;
+
+for (var i = 1; i <= 100; i++) {
+    if ((i % 3 == 0) & (i % 5 == 0)) {
+        console.log(fb);
+        continue;
+    } 
+    if (i % 3 == 0) {
+        console.log(f);
+        continue;
+    }
+    if (i % 5 == 0) {
+        console.log(b);
+        continue;
+    }
     console.log(i);
-    if((i % 3 == 0) && (i % 5 == 0)){
-        console.log("FizzBuzz")
-    }
-    if (i % 3 == 0){
-        console.log("Fizz");
-    }
-    if (i % 5 == 0){
-        console.log("Buzz")
-    } else{
-        console.log(i)
-    }
 }
